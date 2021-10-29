28aec1f24503bf092d18383b185ae2df5bdc443d
sethsaperstein <sethsaper@gmail.com>
2021-10-29 01:48:58 -0400

    switch to 80 so we can forward to 8000

--- src/main.tf
+++ src/main.tf
@@ -30,8 +30,8 @@ resource "aws_security_group" "airbyte_instance" {
   ingress = [
     {
       description      = "allow alb traffic"
-      from_port        = 8000
-      to_port          = 8000
+      from_port        = 80
+      to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = []
       ipv6_cidr_blocks = []
@@ -75,7 +75,7 @@ resource "aws_instance" "this" {
 
 resource "aws_lb_target_group" "this" {
   name     = "${var.project_name}-airbyte-tg-${var.stack_id}"
-  port     = 8000
+  port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
@@ -83,7 +83,7 @@ resource "aws_lb_target_group" "this" {
 resource "aws_lb_target_group_attachment" "this" {
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = aws_instance.this.id
-  port             = 8000
+  port             = 80
 }
 
 resource "aws_security_group" "airbyte_lb" {
