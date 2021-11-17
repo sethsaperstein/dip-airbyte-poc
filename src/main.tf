fc2339d30580b263e0b4b82010ac910b00274b3e
Seth Saperstein <42792771+sethsaperstein@users.noreply.github.com>
2021-11-16 23:08:03 -0500

    add cw logging to containers (#1)

--- src/main.tf
+++ src/main.tf
@@ -48,6 +48,23 @@ locals {
   vpc_id              = data.terraform_remote_state.vpc.outputs.vpc.id
 }
 
+resource "aws_cloudwatch_log_group" "airbyte" {
+  name = "${var.project_name}-airbyte-${var.stack_id}"
+}
+
+resource "aws_s3_bucket" "config" {
+  bucket = "${var.project_name}-airbyte-conf-${var.env_name}-${var.stack_id}-${local.account_id}"
+}
+
+resource "aws_s3_bucket_object" "docker_compose" {
+  bucket = aws_s3_bucket.config.id
+  key    = "docker-compose.yaml"
+  content = templatefile("configs/docker-compose.tpl", {
+    REGION    = "us-east-1",
+    LOG_GROUP = aws_cloudwatch_log_group.airbyte.name,
+  })
+}
+
 resource "aws_security_group" "airbyte_instance" {
   name        = "${var.project_name}-airbyte-sg-${var.stack_id}"
   description = "Sg for hosting Airbyte"
@@ -83,12 +100,57 @@ resource "aws_security_group" "airbyte_instance" {
 }
 
 resource "aws_network_interface" "airbyte" {
-  subnet_id = local.private_subnet_id_1
+  subnet_id       = local.private_subnet_id_1
   security_groups = [aws_security_group.airbyte_instance.id]
 
   tags = {
     Name = "${var.project_name}-airbyte-eni-${var.stack_id}"
   }
+}
+
+resource "aws_iam_role" "instance" {
+  name = "${var.project_name}-airbyte-${var.stack_id}"
+
+  inline_policy {
+    name = "${var.project_name}-airbyte-instance-${var.stack_id}"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["s3:GetObject"]
+          Effect   = "Allow"
+          Resource = ["${aws_s3_bucket.config.arn}/*"]
+        },
+        {
+          Action   = ["cloudwatch:CloudWatchLogsFullAccess"]
+          Effect   = "Allow"
+          Resource = [aws_cloudwatch_log_group.airbyte.arn]
+        }
+      ]
+    })
+  }
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "airbyte" {
+  name = "${var.project_name}-airbyte-${var.stack_id}"
+  role = aws_iam_role.instance.name
 }
 
 resource "aws_instance" "this" {
@@ -101,10 +163,12 @@ resource "aws_instance" "this" {
   }
 
   user_data = templatefile("scripts/startup.tpl", {
-    NGINX_USERNAME = var.nginx_username,
-    NGINX_PASSWORD = var.nginx_password,
+    NGINX_USERNAME   = var.nginx_username,
+    NGINX_PASSWORD   = var.nginx_password,
+    CONF_BUCKET_NAME = aws_s3_bucket.config.id,
   })
-  # key_name = var.airbyte_key_pair_name
+  # TODO: Remove this
+  key_name = var.airbyte_key_pair_name
   tags = {
     Name = "${var.project_name}-airbyte-${var.stack_id}"
   }
